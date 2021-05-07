f6ed45e106028ea10894f6bda3f9a1e0535b0a2c
fede-fede <fede.tamy@gmail.com>
2021-05-07 20:54:00 +0200

    tata

--- Raspberry/jsonhandler.py
+++ Raspberry/jsonhandler.py
@@ -20,7 +20,7 @@ tosend = 0
 tosend = 0
 for i in range(10):
     try:
-        arduino = serial.Serial('COM'+str(i), 2000000, timeout=REC_RATE) #CHANGE FOR RASPBERRY
+        arduino = serial.Serial('COM'+str[i], 2000000, timeout=REC_RATE) #CHANGE FOR RASPBERRY
         
         break
     except:
