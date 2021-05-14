7a3a965b341e554db183e2706e1e53797b22f484
Osiman99 <davideosimo@gmail.com>
2021-05-15 01:59:22 +0200

    update

--- Raspberry/games_simon.py
+++ Raspberry/games_simon.py
@@ -1,26 +1,35 @@
 """All the code has to be implemented with the music and buttons"""
 
+from serial.serialwin32 import Serial
 import jsonhandler
 import random
 import time
 import keyboard
 import winsound
 
+
+
 j = 0
 i = 0
-prevled = 4
-first_start = True
+prevled = 4 
+ready = True
 playing = False
 prevtime = 0
 LIGHTS_CLOCK = 0.2
-next_start = False
-sound = False
+go = False
+set = False
+ledValues = [0,0,0,0,0,0,0,0]
+ledList = []
+DO = 522 
+MI = 658 
+SOL = 784 
+SI = 986
 
 
 """Game of light to introduce the main game"""
 def loop():
     
-    global prevled, first_start, i, j, playing, prevtime, next_start, sound
+    global prevled, ready, i, j, playing, prevtime, go, set,ledValues, ledList
     
     try:
         if keyboard.is_pressed('x'):
@@ -31,9 +40,8 @@ def loop():
             
             currtime = time.time()
             
-            """Game of lights"""
-            if first_start == True:
-                ledValues = [0,0,0,0,0,0,0,0]
+            """Game of lights -READY"""
+            if ready == True:
                 r = random.randint(4,7)
                 for j in range(4, 7, 1):
                     if j == r:
@@ -41,47 +49,66 @@ def loop():
                     else:
                         ledValues[j] = 0
                 jsonhandler.send({"led": ledValues})
-            if (currtime - prevtime) >= LIGHTS_CLOCK and next_start == False:
-                first_start = True
+                if r == 4:
+                    winsound.Beep(DO, 50)
+                if r == 5:
+                    winsound.Beep(MI, 50)
+                if r == 6:
+                    winsound.Beep(SOL, 50)
+                if r == 7:
+                    winsound.Beep(SI, 50)
+            if (currtime - prevtime) >= LIGHTS_CLOCK and go == False:
+                ready = True
                 prevtime = currtime
                 i += 1
             else:
-                first_start = False
-            if i == 12 and first_start == True:
-                first_start = False
-                next_start = True
-                sound = True
-                i = 0
+                ready = False
+            if i == 12:
                 ledValues = [0,0,0,0,0,0,0,0]
                 jsonhandler.send({"led": ledValues})
+                ready = False
+                go = True
+                if (currtime - prevtime) >= LIGHTS_CLOCK+0.3:
+                    set = True
+                    i = 0
+                
             
 
 
-            """3...2...1...GO! (to implement)"""
-            if sound == True:    
-                winsound.Beep(522, 600)
-                time.sleep(0.3)
-                winsound.Beep(522, 600)
-                time.sleep(0.3)
-                winsound.Beep(522, 600)
-                time.sleep(0.3)
-                winsound.Beep(522*2, 750)
-                sound = False
+            """3...2...1...start! -SET"""
+            if set == True:
+                if (currtime - prevtime) >= 1 and i < 3:
+                    winsound.Beep(DO, 200)
+                    i += 1
+                    prevtime = currtime
+                if (currtime - prevtime) >= 1 and i == 3:
+                    winsound.Beep(DO*2, 300)
+                    set = False
+                    prevtime = currtime
 
 
-            """Game starts"""
-            if next_start == True:
-                r = random.randint(4,7)
-                for j in range(4, 7, 1):
-                    if j == r:
-                        ledValues[j] = 1
-                    else:
-                        ledValues[j] = 0
-                jsonhandler.send({"led": ledValues})
-            if (currtime - prevtime) >= LIGHTS_CLOCK and next_start == False:
-                first_start = True
-                prevtime = currtime
-                i += 1
+            """Game starts -GO""" #da fare/completare
+            if go == True:
+                if (currtime - prevtime) >= 1 and i == 3:
+                    
+                    #for k in range(j):
+                        
+                    r = random.randint(4,7)
+                    for j in range(4, 7, 1):
+                        if j == r:
+                            ledValues[j] = 1
+                        else:
+                            ledValues[j] = 0
+                    jsonhandler.send({"led": ledValues})
+                    
+                    #for j in range(30):
+                        #ledList[j] =
+
+
+                #if (currtime - prevtime) >= LIGHTS_CLOCK and go == False:
+                 #   ready = True
+                  #  prevtime = currtime
+                   # i += 1
 
 
 
