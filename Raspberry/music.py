22f35ce685c294e808ac486008142e9ce5206cb9
fede-fede <fede.tamy@gmail.com>
2021-12-01 09:02:02 +0100

    music

--- Raspberry/music.py
+++ Raspberry/music.py
@@ -1,4 +1,50 @@
-Speak("Premi martello per spegnere la musica") #dopo 1 minuto torna al main
-Speak("Premi nota musicale per play o pausa")
-Speak("Premi microfono per avanti")
-Speak("Premi piede per indietro")
+import pygame
+from pygame import mixer
+import keyboard
+import time
+
+pygame.init()
+pygame.mixer.init()
+
+music = ["UnVeroAmicoInMe.wav", "GIORGIA_CREDO.wav", "Toploader_DancingintheMoonlight.wav" ]
+nbSongs = 3
+songID = 0
+playing = False
+status = False
+pygame.mixer.music.load(music[songID])
+
+
+while True:  
+    try:  
+        if keyboard.is_pressed('p') and not playing :
+            playing =True
+            time.sleep(0.3)
+            print ("stopped->playing")
+            print (songID)
+            print (music[songID])
+            pygame.mixer.music.play()
+            
+        #pygame.mixer.music.get_busy()--> Returns True when the music stream is actively playing. When the music is idle this returns False.
+        if playing and pygame.mixer.music.get_busy() == False:
+            break
+    
+        if keyboard.is_pressed('p')  and playing  :
+            time.sleep(0.3)
+            pygame.mixer.music.stop()
+            print ("playing->stopped")
+            songID += 1
+            print (songID)
+            print (music[songID])
+
+            if songID == nbSongs:
+                playing = False
+                songID = 0
+                pygame.mixer.music.stop()
+                break
+
+            pygame.mixer.music.load(music[songID])
+            pygame.mixer.music.play()
+            
+
+    except:
+        break 
